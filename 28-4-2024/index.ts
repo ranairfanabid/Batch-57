37a926b16be3198853ecd11e3cc282f35cf19c7d
Irfan Abid <ranairfanabid@gmail.com>
2024-04-30 16:30:10 +0500

    30-4-2024

---
+++ 28-4-2024/index.ts
@@ -0,0 +1,15 @@
+//loops and types  
+//do while loop-- (conditon)// run and then check condition afterwards
+//while loop-- check condition and then execute
+//for loop-- check condition and then execute
+
+//for(exp; exp2; exp3)}{}
+//exp1:starting condition (let variable=0) it depends on you from where you want to start value
+//exp2: ending condition(variable<5)
+//step 1 condition
+//stepp 2 true then execute else terminate
+//step increment or decrement
+for (let x=0; x<5;x++) {
+    
+console.log("My Name is Irfan")
+}
--- Project Calculator/index.js
+++ Project Calculator/index.js
@@ -9,5 +9,16 @@ const name1 = inquirer_1.default.prompt([
         type: "input",
         name: "First number",
         message: "enter your first numvber"
+    },
+    {
+        type: "input",
+        name: "second number",
+        message: "enter your second numvber"
+    },
+    {
+        type: "operator",
+        name: "Operator",
+        choices: ["+", "-", "/", "*"],
+        message: "Select operator"
     }
 ]);
