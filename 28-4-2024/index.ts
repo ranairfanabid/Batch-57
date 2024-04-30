304a4e49be214135b6030c5a9e1798cce242ce88
Irfan Abid <ranairfanabid@gmail.com>
2024-04-30 16:35:16 +0500

    30-4-2024

--- 28-4-2024/index.ts
+++ 28-4-2024/index.ts
@@ -1,15 +1,126 @@
-//loops and types  
-//do while loop-- (conditon)// run and then check condition afterwards
-//while loop-- check condition and then execute
-//for loop-- check condition and then execute
+// //loops and types  
+// //do while loop-- (conditon)// run and then check condition afterwards
+// //while loop-- check condition and then execute
+// //for loop-- check condition and then execute
 
-//for(exp; exp2; exp3)}{}
-//exp1:starting condition (let variable=0) it depends on you from where you want to start value
-//exp2: ending condition(variable<5)
-//step 1 condition
-//stepp 2 true then execute else terminate
-//step increment or decrement
-for (let x=0; x<5;x++) {
+// //for(exp; exp2; exp3)}{}
+// //exp1:starting condition (let variable=0) it depends on you from where you want to start value
+// //exp2: ending condition(variable<5)
+// //step 1 condition
+// //stepp 2 true then execute else terminate
+// //step increment or decrement
+// // for (let x=0; x<5;x++) {
     
-console.log("My Name is Irfan")
-}
+// // console.log("My Name is Irfan", x)
+// // }
+// // let x1=["a","b","c", "d"]
+
+// // for (let x=0;x<x1.length;x++){
+// //     console.log(x1)
+// // }
+
+// // function Table(input:number){
+// //     for(let x=1;x<=10;x++){
+// // console.log(`${input} x ${x} = ${input*x}`)
+// //     }
+// // }
+// // Table(2) 
+
+// // console.log("Hello World");
+
+// // let temperature:number=2
+// //     if (temperature<20) 
+// //     {console.log("It is cold")}
+// //         else console.log("it is not cold")
+     
+        
+// // let apple = 10
+// // console.log(apple-3)
+// // 2
+// // function calculatearea(input:number){
+// //     console.log(input*6.14)
+// // }
+// // calculatearea(3)
+
+// // for (let a=1; a<=50; a++)
+// // if (a%3==0)(console.log("Fizz"))
+// // else if (a%5==0) (console.log("buzz")) 
+// //     else console.log(a);
+    
+// // let a=5
+
+// // if (a>5) console.log("True");
+// // else console.log(false);*//
+
+// //function irfan(input:number){
+// //for (let x2=1; x2<11; x2++){
+//    // console.log(`${input}x${x2}=${input*x2}` )
+// //}    
+// //}
+// //irfan(10)
+
+// let x2=10
+// if (x2!=10) console.log(("True"));
+// if (x2==9) console.log(("True"));
+// else console.log(x2);
+
+
+
+
+// function greet(name: string) {
+//     console.log("Hello, " + name.toUpperCase() + "!!");
+//   }
+//   greet("My Name is irfan")
+
+//   function greet1(){
+//    console.log("my name is irfan");
+   
+//   }
+//   greet1()
+
+//   function greetWithParameter(name: string) {
+//    console.log("Hello there ", name);
+//  }
+ 
+//  greetWithParameter("Jahanzaib");
+//  greetWithParameter("Hassan");
+//  greetWithParameter("Ali");
+//  greetWithParameter("Mubbara");
+
+//  const fruits = ["apple", "banana", "orange"];
+// const numbers = [1, 2, 3];
+// console.log(fruits[1]);
+// console.log("Array Length", fruits.length);
+//  let n=15
+//  n+=15
+//  n++
+//  n=n+2
+//  n++
+//  if (n==34 && n==34)  console.log(true);
+ 
+//  else console.log(n);
+ 
+// if (n==0){
+// console.log ("true"); 
+// }
+
+//    else console.log("false");
+ 
+
+
+
+//    for (let xy=1; xy<=50; xy++)
+//    if (xy%2==0) (console.log("Irfan"))
+//    else console.log(xy);
+
+   
+
+// for (let b=1; b<=50; b++)
+//    if (b%2==0)(console.log("even"))
+//    //else if (a%5==0) (console.log("buzz")) 
+//     else console.log("odd");
+       
+
+let a20=20
+if (a20%3==0) console.log("true");
+else("false")
