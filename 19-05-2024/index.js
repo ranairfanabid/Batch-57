bc37564c64f1333502437812a2edae27df949eab
Irfan Abid <ranairfanabid@gmail.com>
2024-06-03 08:49:43 +0500

    19-5-2024

---
+++ 19-05-2024/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// let obj={name:"irfan", age:38, height:6}
+// console.log(obj);
+// console.log(obj.age);
+// function year1(input:number) {
+// for (let i=0; i<input.lenght; i++){
+// if (i>=input)console.log(true);
+// else console.log(false);
+// }    
+// }
+// const abc11:number = [0,0,10,2,3];
+// console.log(year1(abc11));
+function abc(input) {
+    for (let a = 0; a <= 20; a++)
+        abc(20);
+}
